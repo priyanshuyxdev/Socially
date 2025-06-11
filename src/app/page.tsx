dfcbcf35166d281c59c4864cd076842402ecbd4d
priyanshuyxdev <priyanshuyxdev@gmail.com>
2025-06-11 23:45:02 +0530

    auth & db sync

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,6 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  UserButton,
-} from "@clerk/nextjs";
-import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+  
   return (
     <div className="m-4">
       <h1>home page content</h1>
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,22 @@
-// import { Link } from "lucide-react"
 import Link from "next/link";
 import DesktopNavbar from "./DesktopNavbar";
 import MobileNavbar from "./MobileNavbar";
+import { currentUser } from "@clerk/nextjs/server";
+import { syncUser } from "@/actions/user.action";
 
-
-function Navbar() {
+async function Navbar() {
+  const user = await currentUser()
+  if(user) await syncUser();
   return (
     <nav className="sticky top-0 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex items-center justify-between   h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-xl font-bold text-primary font-mono tracking-wider">
-            Socially
+            <Link
+              href="/"
+              className="text-xl font-bold text-primary font-mono tracking-wider"
+            >
+              Socially
             </Link>
           </div>
 
@@ -20,7 +25,7 @@ function Navbar() {
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
